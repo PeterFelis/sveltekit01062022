44800e89cc3a70898347635fab82e858231fa5fc
Peter Felis <Peter@felis.nl>
2022-06-01 22:28:22 +0200

    met personen

---
+++ src/routes/personen.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { supabase } from '$lib/supabaseClient';
+
+let naam;
+let leeftijd;
+
+//let { error } = await supabase.from('profiles').upsert(updates, {
+//        returning: 'minimal' // Don't return the value after inserting
+//      });
+
+
+const invoeren = async()=>{
+    await supabase.from('personen')
+        .insert([{Naam:naam, Leeftijd:leeftijd}])
+
+    naam="";
+    leeftijd="";  
+}
+
+</script>
+
+
+<input type="text" placeholder="naam" bind:value={naam}/>
+<input type="number" placeholder="leeftijd" bind:value="{leeftijd}"/>
+<input type="button" on:click="{invoeren}"/>
