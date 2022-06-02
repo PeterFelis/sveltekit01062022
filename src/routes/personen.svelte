bad965995e56993d62152e0f2bc0fdf7ea0bfa86
Peter Felis <Peter@felis.nl>
2022-06-02 03:03:48 +0200

    pff laat

--- src/routes/personen.svelte
+++ src/routes/personen.svelte
@@ -1,25 +1,48 @@
 <script>
+
     import { supabase } from '$lib/supabaseClient';
 
 let naam;
 let leeftijd;
 
-//let { error } = await supabase.from('profiles').upsert(updates, {
-//        returning: 'minimal' // Don't return the value after inserting
-//      });
 
+const tonen = async function() {
+    const {data,error} = await supabase.from('personen').select();
+    return data;
+};
 
-const invoeren = async()=>{
+const invoeren = async () =>{
     await supabase.from('personen')
-        .insert([{Naam:naam, Leeftijd:leeftijd}])
-
+        .insert({Naam:naam, Leeftijd:leeftijd})
     naam="";
-    leeftijd="";  
+    leeftijd="";
+};
+    
+let promise = tonen;
+
+function ophalen() {
+    promise = tonen();  
+}
+
+function weergeven(){
+    invoeren();
 }
 
 </script>
 
 
+
 <input type="text" placeholder="naam" bind:value={naam}/>
 <input type="number" placeholder="leeftijd" bind:value="{leeftijd}"/>
-<input type="button" on:click="{invoeren}"/>
+<input type="button" on:click={ophalen} value="versturen"/>
+
+
+<input type="button" on:click={tonen} value="tonen"/>
+
+{#await tonen}
+	<p>...waiting</p>
+{:then data}
+	<img src={data.message} alt="Dog image" />
+{:catch error}
+	<p>An error occurred!</p>
+{/await}
