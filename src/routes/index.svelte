c0b963f06af32db911dde5816f1e2c35d9a4fd65
Peter Felis <peter@felis.nl>
2022-09-16 08:42:08 +0200

    vrijdag

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -53,8 +53,8 @@
   >
     <div class="text-5xl">Sorry!!</div>
     <div class="mt-8">
-      Het duurt wat langer.
-      <br /> Op 8 september zijn we weer online
+      Het duurt wat langer. Plannen blijft moeilijk..
+      <br /> Op 23 september zijn we weer online
       <br /> Even bellen kan ook: 0174 769-132 <br />(lossen we het meteen op)
     </div>
   </div>
